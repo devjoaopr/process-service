5e2260674ee42c1b2da20e6360674ca3829eca44
devjoaopr <joaoprsantos1@outlook.com>
2026-07-02 11:23:40 -0300

    idk

--- src/main/java/com/process_service/dto/Locator/LocatorDTO.java
+++ src/main/java/com/process_service/dto/Locator/LocatorDTO.java
@@ -1,19 +1,18 @@
 package com.process_service.dto.Locator;
 
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+@Builder
 public record LocatorDTO(
         UUID id,
         @Size(max = 150)
         String name,
         @Size(max = 150)
         String slug,
-        String description,
-        Boolean active,
-        Integer displayOrder,
         OffsetDateTime createdAt,
         OffsetDateTime updatedAt,
         OffsetDateTime deletedAt
